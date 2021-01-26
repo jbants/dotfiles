ab1c5634e39c8571de9028921eec04d5d665dc4f
James Banting <jrbanting@gmail.com>
2021-01-25 20:04:22 -0800

    added a different alias and gitconfig

--- install.sh
+++ install.sh
@@ -100,6 +100,10 @@ setup_symlinks() {
     done
 }
 
+
+install_omzsh() {
+    title "Installing ohmyzsh"
+}
 # # Add this back in  - JRB
 # setup_git() {
 #     title "Setting up Git"
@@ -144,26 +148,6 @@ setup_symlinks() {
         echo "default shell changed to $zsh_path"
     fi
 }
-
-# if ! command_exists zsh; then
-#     echo "zsh not found. Please install and then re-run installation scripts"
-#     exit 1
-# elif ! [[ $SHELL =~ .*zsh.* ]]; then
-#     echo "Configuring zsh as default shell"
-#     chsh -s "$( command -v zsh)"
-# fi
-
-# # Change the default shell to zsh
-# zsh_path="$( command -v zsh )"
-# if ! grep "$zsh_path" /etc/shells; then
-#     echo "adding $zsh_path to /etc/shells"
-#     echo "$zsh_path" | sudo tee -a /etc/shells
-# fi
-
-# if [[ "$SHELL" != "$zsh_path" ]]; then
-#     chsh -s "$zsh_path"
-#     echo "default shell changed to $zsh_path"
-# fi
 
 case "$1" in
     backup)
