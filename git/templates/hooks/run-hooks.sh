c5050c9e7fdee262065f7f52990bb66368803857
James Banting <jrbanting@gmail.com>
2021-02-15 22:12:17 -0800

    need to sort out git and update conda envs

--- git/templates/hooks/run-hooks.sh
+++ git/templates/hooks/run-hooks.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-EXIT_CODE=0
+# EXIT_CODE=0
 
-repo=$( git rev-parse --show-toplevel )
-hook_type=$( basename $0 )
-hooks=~/.dotfiles/git/hooks
+# repo=$( git rev-parse --show-toplevel )
+# hook_type=$( basename $0 )
+# hooks=~/.dotfiles/git/hooks
 
-shopt -s nullglob
-for hook in $hooks/*.$hook_type; do
-	echo ""
-	echo "${COLOR_LIGHTPURPLE}Executing ${hook}${COLOR_NONE}"
-	${hook}
-	EXIT_CODE=$((${EXIT_CODE} + $?))
-done
+# shopt -s nullglob
+# for hook in $hooks/*.$hook_type; do
+# 	echo ""
+# 	echo "${COLOR_LIGHTPURPLE}Executing ${hook}${COLOR_NONE}"
+# 	${hook}
+# 	EXIT_CODE=$((${EXIT_CODE} + $?))
+# done
 
-if [[ ${EXIT_CODE} -ne 0 ]]; then
-	echo ""
-	echo "${COLOR_RED}Commit Failed.${COLOR_NONE}"
-fi
+# if [[ ${EXIT_CODE} -ne 0 ]]; then
+# 	echo ""
+# 	echo "${COLOR_RED}Commit Failed.${COLOR_NONE}"
+# fi
 
-exit $((${EXIT_CODE}))
+# exit $((${EXIT_CODE}))
